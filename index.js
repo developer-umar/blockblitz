597aa21476643a70ace5675a814db11ce0518fc7
Mohammad Umar <mohammadumar8010@gmail.com>
2025-10-21 21:17:46 +0530

    taking index.js out of the dirctory

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { app } from "./app.js"
+import { app } from "./src/app.js"
 
 import dotenv from "dotenv"
-import { connectDB } from "./db/index.js"
+import { connectDB } from "./src/db/index.js"
 
 
 dotenv.config({path:'./.env'})
