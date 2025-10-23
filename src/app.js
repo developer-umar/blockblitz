579c5c3ba2cf5a6a80fc973e9394774c6eca3d66
Mohammad Umar <mohammadumar8010@gmail.com>
2025-10-23 20:49:06 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 app.use(express.json());   
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
