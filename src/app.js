bdc5bebfaedcc47b915710254aa209c464865e1c
Mohammad Umar <mohammadumar8010@gmail.com>
2025-10-23 20:18:12 +0530

    added cors for postman

--- src/app.js
+++ src/app.js
@@ -7,10 +7,16 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin:"http://localhost:5173",
-    credentials: true
-}))
+  origin: function(origin, callback) {
+    // origin null (Postman) ya localhost:5173 → allow
+    if (!origin || origin === "http://localhost:5173") {
+      callback(null, true);
+    } else {
+      callback(null, true); // filhaal sab allow karna
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());   
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
