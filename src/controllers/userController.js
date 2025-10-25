3937c5f4c183e440a62a99e7e4f52c4b3a4e3708
Mohammad Umar <mohammadumar8010@gmail.com>
2025-10-25 16:52:27 +0530

    added secure false for developemnt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -131,7 +131,7 @@ export const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite: "none",
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
