e11179abb018715cb53cde3bf7688f05a3669960
Mohammad Umar <mohammadumar8010@gmail.com>
2025-12-02 00:19:04 +0530

    fix: cors issues

--- src/app.js
+++ src/app.js
@@ -7,10 +7,13 @@ dotenv.config();  // Load .env
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+  origin: ["https://blogblitz-gamma.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
+app.options("*", cors()); // 🔥 CORS preflight fix
 app.use(express.json());   
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
